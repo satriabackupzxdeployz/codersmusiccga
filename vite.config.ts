0d94029a60f3b9b38ccc291ea4058d4a37f03036
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-06-24 20:14:45 +0700

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    minify: false,
+    cssMinify: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          if (id.includes("src/utils/")) {
+            return "utils";
+          }
+        },
+      },
+    },
+  },
+});
